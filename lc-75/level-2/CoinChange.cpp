799ab3a43ce7e9e63f039129e429aa7b8fec5111
Pritam Nikam <pritam.nikam@gmail.com>
2022-07-27 19:09:56 +0530

    Update CoinChange.cpp

--- lc-75/level-2/CoinChange.cpp
+++ lc-75/level-2/CoinChange.cpp
@@ -46,7 +46,8 @@ namespace {
 
     class Solution {
     public:
-        int coinChange(vector<int>& coins, int amount) {
+        // Space: O(n * s), Time: O(n * s)
+        int coinChange2D(vector<int>& coins, int amount) {
             int n = coins.size();
             vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
 
@@ -79,6 +80,21 @@ namespace {
 
             return (dp[n - 1][amount] == numeric_limits<int>::max()) 
                         ? -1 : dp[n - 1][amount];
+        }
+        
+        // Space: O(n), Time: O(n * s)
+        int coinChange(vector<int>& coins, int amount) {
+            vector<int> dp(amount + 1, amount + 1);
+            dp[0] = 0;
+
+            for (int i = 1; i <= amount; ++i) {
+                for (int coin: coins){
+                    if (i - coin < 0) continue;
+
+                    dp[i] = min(dp[i], dp[i-coin] + 1);
+                }
+            }
+            return dp[amount] == amount + 1 ? -1 : dp[amount];
         }
     };
 } // namesapce
