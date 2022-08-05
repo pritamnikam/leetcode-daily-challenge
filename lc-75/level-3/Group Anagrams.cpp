3e264d96309581f58fc083322aa5a981fc5ef952
Pritam Nikam <pritam.nikam@gmail.com>
2022-08-05 12:18:52 +0530

    Create Group Anagrams.cpp

---
+++ lc-75/level-3/Group Anagrams.cpp
@@ -0,0 +1,77 @@
+/*
+49. Group Anagrams
+https://leetcode.com/problems/group-anagrams/
+
+Given an array of strings strs, group the anagrams together. 
+You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a
+different word or phrase, typically using all the original letters exactly once.
+
+
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+namespace {
+    class Solution {
+    public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            vector<vector<string>> result;
+            if (strs.empty()) return result;
+
+            const int kSize = 26;
+            unordered_map<string, vector<string>> stringToSetMap;
+
+            for (string str : strs) {
+                vector<int> charFreqMap(kSize, 0);
+                for (char ch : str) charFreqMap[ch - 'a']++;
+
+                string key;
+                for (int i = 0; i < kSize; ++i) {
+                    key += "#";
+                    key += to_string(charFreqMap[i]);
+                }
+
+                stringToSetMap[key].emplace_back(str);
+            }
+
+            for (auto it : stringToSetMap) {
+                result.push_back(it.second);
+            }
+
+            return result;
+        }
+    };
+}  // namespace
+
+void testGroupAnagrams() {
+    vector<string> strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+    vector<vector<string>> result = Solution().groupAnagrams(strs);
+}
