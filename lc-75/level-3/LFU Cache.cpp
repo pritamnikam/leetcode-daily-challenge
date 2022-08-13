d2fb0e687d4aa18d1ec81f58ec9c61c856468122
Pritam Nikam <pritam.nikam@gmail.com>
2022-08-13 08:58:36 +0530

    Create LFU Cache.cpp

---
+++ lc-75/level-3/LFU Cache.cpp
@@ -0,0 +1,126 @@
+/*
+460. LFU Cache
+https://leetcode.com/problems/lfu-cache/submissions/
+
+Design and implement a data structure for a Least Frequently Used (LFU) cache.
+
+Implement the LFUCache class:
+
+LFUCache(int capacity) Initializes the object with the capacity of the data structure.
+int get(int key) Gets the value of the key if the key exists in the cache. Otherwise, returns -1.
+void put(int key, int value) Update the value of the key if present, or inserts the key if not already present. When the cache reaches its capacity, it should invalidate and remove the least frequently used key before inserting a new item. For this problem, when there is a tie (i.e., two or more keys with the same frequency), the least recently used key would be invalidated.
+To determine the least frequently used key, a use counter is maintained for each key in the cache. The key with the smallest use counter is the least frequently used key.
+
+When a key is first inserted into the cache, its use counter is set to 1 (due to the put operation). The use counter for a key in the cache is incremented either a get or put operation is called on it.
+
+The functions get and put must each run in O(1) average time complexity.
+
+
+
+Example 1:
+Input
+["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+
+Explanation
+// cnt(x) = the use counter for key x
+// cache=[] will show the last used order for tiebreakers (leftmost element is  most recent)
+LFUCache lfu = new LFUCache(2);
+lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+lfu.get(1);      // return 1
+                 // cache=[1,2], cnt(2)=1, cnt(1)=2
+lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
+                 // cache=[3,1], cnt(3)=1, cnt(1)=2
+lfu.get(2);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,1], cnt(3)=2, cnt(1)=2
+lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
+                 // cache=[4,3], cnt(4)=1, cnt(3)=2
+lfu.get(1);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,4], cnt(4)=1, cnt(3)=3
+lfu.get(4);      // return 4
+                 // cache=[4,3], cnt(4)=2, cnt(3)=3
+
+
+Constraints:
+0 <= capacity <= 104
+0 <= key <= 105
+0 <= value <= 109
+At most 2 * 105 calls will be made to get and put.
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <unordered_map>
+#include <list>
+
+namespace {
+
+    class LFUCache {
+        int cap;
+        int size;
+        int minFreq;
+        unordered_map<int, pair<int, int>> m; //key to {value,freq};
+        unordered_map<int, list<int>::iterator> mIter; //key to list iterator;
+        unordered_map<int, list<int>>  fm;  //freq to key list;
+
+    public:
+        LFUCache(int capacity) {
+            cap = capacity;
+            size = 0;
+        }
+
+        int get(int key) {
+            if (m.count(key) == 0) return -1;
+
+            fm[m[key].second].erase(mIter[key]);
+            m[key].second++;
+            fm[m[key].second].push_back(key);
+            mIter[key] = --fm[m[key].second].end();
+
+            if (fm[minFreq].size() == 0)
+                minFreq++;
+
+            return m[key].first;
+        }
+
+        void put(int key, int value) {
+            if (cap <= 0) return;
+
+            int storedValue = get(key);
+            if (storedValue != -1)
+            {
+                m[key].first = value;
+                return;
+            }
+
+            if (size >= cap)
+            {
+                m.erase(fm[minFreq].front());
+                mIter.erase(fm[minFreq].front());
+                fm[minFreq].pop_front();
+                size--;
+            }
+
+            m[key] = { value, 1 };
+            fm[1].push_back(key);
+            mIter[key] = --fm[1].end();
+            minFreq = 1;
+            size++;
+        }
+    };
+
+    /**
+     * Your LFUCache object will be instantiated and called as such:
+     * LFUCache* obj = new LFUCache(capacity);
+     * int param_1 = obj->get(key);
+     * obj->put(key,value);
+     */
+
+}
