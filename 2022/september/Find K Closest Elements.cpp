bdabf569069c4affce27f9d55e1f4d85962237d3
Pritam Nikam <pritam.nikam@gmail.com>
2022-09-29 06:56:27 +0530

    Create Find K Closest Elements.cpp

---
+++ 2022/september/Find K Closest Elements.cpp
@@ -0,0 +1,65 @@
+/*
+658. Find K Closest Elements
+https://leetcode.com/problems/find-k-closest-elements/
+
+Given a sorted integer array arr, two integers k and x, return the k closest
+integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+
+
+Example 1:
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+
+Example 2:
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+
+
+Constraints:
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr is sorted in ascending order.
+-104 <= arr[i], x <= 104
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+
+namespace {
+    class Solution {
+    public:
+        vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+            if (arr.empty()) return {};
+            vector<int> ans;
+            priority_queue<
+                pair<int, int>
+            > maxHeap;
+
+            // add to max heap {diff, num}
+            for (int n : arr) {
+                maxHeap.push({ abs(n - x), n });
+                if (maxHeap.size() > k) maxHeap.pop();
+            }
+
+            // pop the result to ans array
+            while (!maxHeap.empty()) {
+                ans.push_back(maxHeap.top().second);
+                maxHeap.pop();
+            }
+
+            // sort in ascending order.
+            sort(begin(ans), end(ans));
+            return ans;
+        }
+    };
+}  // namespace 
