ac823054aec43c74ed902085f9d5b561f58c52c3
Pritam Nikam <pritam.nikam@gmail.com>
2022-10-03 09:20:39 +0530

    Update Number of Dice Rolls With Target Sum.cpp

--- 2022/october/Number of Dice Rolls With Target Sum.cpp
+++ 2022/october/Number of Dice Rolls With Target Sum.cpp
@@ -6,7 +6,7 @@ You have n dice and each die has k faces numbered from 1 to k.
 
 Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 109 + 7.
 
- 
+
 
 Example 1:
 
@@ -25,7 +25,7 @@ Example 3:
 Input: n = 30, k = 30, target = 500
 Output: 222616187
 Explanation: The answer must be returned modulo 109 + 7.
- 
+
 
 Constraints:
 
@@ -34,37 +34,91 @@ Constraints:
 
 */
 
+
 using namespace std;
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <math.h>
 
 namespace {
-  class Solution {
-    static const int mod = pow(10, 9) + 7;
-    
-  public:
-    
-      int numRollsToTarget(int d, // no. of dice
-                           int f, // faces on dice [1...f]
-                           int target) {
-          vector<int> ways(target + 1);
-          ways[0] = 1;
-          
-          // for all dices
-          for (int x = 1; x <= d; x++) {
-              // target sum
-              for (int i = target; i >= 0; i--) {
-                  int way = 0;
-                  for (int y = i - 1; y >= max(0, i - f);  y--) {
-                    way = (way + ways[y]) % mod;
-                  }
-                
-                  // update ways to reach target sum
-                  ways[i] = way;
-              }
-          }
-          return ways[target];
-      }
-  };
-} // namespace
+	class Solution {
+	
+		const int mod = pow(10, 9) + 7;
+
+		inline void solve(int f,
+			int target,
+			vector<int>& ways) {
+
+			// target sum:
+			for (int i = target; i >= 0; i--) {
+
+				int way = 0;
+
+				for (int y = i - 1;
+					y >= max(0, i - f);
+					y--) {
+
+					way = (way + ways[y]) % mod;
+				}
+
+				ways[i] = way;
+			}
+		}
+
+	public:
+		int numRollsToTarget(int d,
+			int f,
+			int target) {
+
+			vector<int> ways(target + 1);
+			ways[0] = 1;
+
+			for (int x = 1; x <= d; x++) {
+				solve(f, target, ways);
+			}
+
+			return ways[target];
+		}
+		
+
+private:
+		int numRollsToTargetDP(int d, int f, int target) {
+			vector<int> dp(target + 1, 1);
+			int i, j, k;
+			for (i = 1; i <= d; ++i) {
+				for (k = target; k >= (i == d ? target : 0); --k) {
+					for (j = k - 1, dp[k] = 0; j >= max(0, k - f); --j) {
+						dp[k] = (dp[k] + dp[j]) % 1000000007;
+					}
+				}
+			}
+			return dp[target];
+		}
+
+		int numRollsToTarget2(int n, int k, int target) {
+			long ans = recurse(n, k, target);
+			return ans % 1000000007;
+		}
+
+		long recurse(int n, int k, int target) {
+			if (n <= 0 || k <= 0 || target < n || target > n * k) {
+				return 0;
+			}
+
+			if (n == 1 and k >= target) {
+				return 1;
+			}
+
+			long sum = 0;
+			for (int i = min(target, k); 
+					 i > 0 && ((target - i) <= ((n - 1) * k));
+					 i--) {
+				sum += recurse(n - 1, k, target - i);
+			}
+
+			return sum;
+		}
+	};
+}
