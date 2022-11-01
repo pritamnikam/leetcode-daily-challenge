fa7add73af4b04d9cca5a455cc3280c0c914f0ae
Pritam Nikam <pritam.nikam@gmail.com>
2022-11-01 16:36:01 +0530

    Create Maximum Length of a Concatenated String with Unique Characters.cpp

---
+++ 2022/october/Maximum Length of a Concatenated String with Unique Characters.cpp
@@ -0,0 +1,35 @@
+/*
+
+1239. Maximum Length of a Concatenated String with Unique Characters
+https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+*/
+
+namespace {
+  class Solution {
+  public:
+      int maxLength(vector<string>& arr) {
+          // Use depth first search recursion through arr
+          // building from an initial empty string
+          return dfs(arr, 0, "");
+      }
+
+  private:
+      int dfs(vector<string>& arr, int pos, string res) {    
+          // Use a set to check res for duplicate characters
+          unordered_set<char> resSet(res.begin(), res.end());
+          if (res.size() != resSet.size())
+              return 0;
+
+          // Recurse through each possible next option
+          // and find the best answer
+          int best = res.size();
+          for (int i = pos; i < arr.size(); i++) {
+              best = max(best, 
+                         dfs(arr, i + 1, res + arr[i]));
+          }
+
+          return best;
+      }
+  };
+} // namespace
