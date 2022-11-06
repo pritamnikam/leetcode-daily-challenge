b6853005890a42138c07ef7e229b1603c77c5b6f
Pritam Nikam <pritam.nikam@gmail.com>
2022-11-06 14:53:47 +0530

    Create Orderly Queue.cpp

---
+++ 2022/november/Orderly Queue.cpp
@@ -0,0 +1,67 @@
+/*
+899. Orderly Queue
+https://leetcode.com/problems/orderly-queue/
+
+You are given a string s and an integer k. You can choose one of the first k letters of s and append it at the end of the string..
+
+Return the lexicographically smallest string you could have after applying the mentioned step any number of moves.
+
+Example 1:
+Input: s = "cba", k = 1
+Output: "acb"
+Explanation:
+In the first move, we move the 1st character 'c' to the end, obtaining the string "bac".
+In the second move, we move the 1st character 'b' to the end, obtaining the final result "acb".
+
+Example 2:
+Input: s = "baaca", k = 3
+Output: "aaabc"
+Explanation:
+In the first move, we move the 1st character 'b' to the end, obtaining the string "aacab".
+In the second move, we move the 3rd character 'c' to the end, obtaining the final result "aaabc".
+
+Constraints:
+1 <= k <= s.length <= 1000
+s consist of lowercase English letters.
+
+*/
+
+using namespace std;
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+namespace {
+    class Solution {
+    public:
+        string orderlyQueue(string s, int k) {
+            if (k > 1) {
+                sort(s.begin(), s.end());
+                return s;
+            }
+
+            string res = s;
+            for (int i = 1; i < s.length(); i++) {
+                res = min(
+                    res,
+                    s.substr(i) + s.substr(0, i)
+                );
+            }
+
+            return res;
+        }
+    };
+}  // namespace
+
+
+void testOrderlyQueue() {
+    // Input: s = "cba", k = 1
+    // Output: "acb"
+    cout << "Orderly Queue: " << Solution().orderlyQueue("cba", 1) << endl;
+
+    // Input: s = "baaca", k = 3
+    // Output: "aaabc"
+    cout << "Orderly Queue: " << Solution().orderlyQueue("baaca", 3) << endl;
+}
