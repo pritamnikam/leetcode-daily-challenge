220d4cf20bd3f89d44e30a4b4c63445e923f5930
Pritam Nikam <pritam.nikam@gmail.com>
2022-12-17 16:09:03 +0530

    Update Evaluate Reverse Polish Notation.cpp

--- 2022/december/Evaluate Reverse Polish Notation.cpp
+++ 2022/december/Evaluate Reverse Polish Notation.cpp
@@ -3,27 +3,23 @@
 https://leetcode.com/problems/evaluate-reverse-polish-notation/
 
 Evaluate the value of an arithmetic expression in Reverse Polish Notation.
-
 Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
 
 Note that division between two integers should truncate toward zero.
-
-It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
-
-
+It is guaranteed that the given RPN expression is always valid. That means the expression would
+always evaluate to a result, and there will not be any division by zero operation.
 
 Example 1:
-
 Input: tokens = ["2","1","+","3","*"]
 Output: 9
 Explanation: ((2 + 1) * 3) = 9
-Example 2:
 
+Example 2:
 Input: tokens = ["4","13","5","/","+"]
 Output: 6
 Explanation: (4 + (13 / 5)) = 6
-Example 3:
 
+Example 3:
 Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
 Output: 22
 Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
@@ -34,12 +30,9 @@ Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
 = 17 + 5
 = 22
 
-
 Constraints:
-
 1 <= tokens.length <= 104
 tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
-
 */
 
 using namespace std;
@@ -52,42 +45,56 @@ using namespace std;
 namespace {
 	class Solution {
 	public:
+		int evalRPN1(vector<string>& tokens) {
+		    stack<int> stack;
+		    for (string& token : tokens) {
+			if (isOperator(token)) {
+			    int y = stack.top(); stack.pop();
+			    int x = stack.top(); stack.pop();
+
+			    stack.push(evaluate(token, x, y));
+			}
+			else {
+			    stack.push(stoi(token));
+			}
+		    }
+
+		    return stack.top();
+		}
+
 		int evalRPN(vector<string>& tokens) {
-            stack<int> stack;
-            for (string& token : tokens) {
-                if (isOperator(token)) {
-                    int x = stack.top(); stack.pop();
-                    int y = stack.top(); stack.pop();
-
-                    stack.push(evaluate(token, x, y));
-                }
-                else {
-                    stack.push(stoi(token));
-                }
-            }
-
-            return stack.top();
+		    string t = tokens.back();
+		    tokens.pop_back();
+		    if (t != "+" && t != "-" && t != "*" && t != "/") return stoi(t);
+		    else {
+			int b = evalRPN(tokens);
+			int a = evalRPN(tokens);
+			if (t == "+") return a + b;
+			else if (t == "-") return a - b;
+			else if (t == "*") return a * b;
+			else return a / b;
+		    }
 		}
 
 	private:
-        bool isOperator(const string& str) {
-            if (str.length() > 1) 
-                return false;
+		bool isOperator(const string& str) {
+		    if (str.length() > 1) 
+			return false;
 
-            char operation = str[0];
-            return  operation == '+' 
-                || operation == '-' 
-                || operation == '/' 
-                || operation == '*';
-        }
+		    char operation = str[0];
+		    return  operation == '+' 
+			|| operation == '-' 
+			|| operation == '/' 
+			|| operation == '*';
+		}
 
-        int evaluate(const string& str, int op1, int op2) {
-            char operation = str[0];
-            if (operation == '+') return op1 + op2;
-            else if (operation == '-') return op1 - op2;
-            else if (operation == '/') return op1 / op2;
-            return op1 * op2;
-        }
+		int evaluate(const string& str, int op1, int op2) {
+		    char operation = str[0];
+		    if (operation == '+') return op1 + op2;
+		    else if (operation == '-') return op1 - op2;
+		    else if (operation == '/') return op1 / op2;
+		    return op1 * op2;
+		}
 	};
 }  // namespace
 
@@ -95,5 +102,9 @@ void testEvaluateReversePolishNotation() {
     // Input: tokens = ["2","1","+","3","*"], Output: 9, 
     // Explanation : ((2 + 1) * 3) = 9
     vector<string> tokens = { "2", "1", "+", "3", "*" };
+    cout << "Evaluate Reverse Polish Notation: " << Solution().evalRPN(tokens) << endl;
+
+    // Input: tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"], Output : 22
+    tokens = { "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+" };
     cout << "Evaluate Reverse Polish Notation: " << Solution().evalRPN(tokens) << endl;
 }
