a8867aa23c379705603bacaafc342776a402d4ff
Pritam Nikam <pritam.nikam@gmail.com>
2022-12-30 10:53:23 +0530

    Update All Paths From Source to Target.cpp

--- 2022/december/All Paths From Source to Target.cpp
+++ 2022/december/All Paths From Source to Target.cpp
@@ -71,4 +71,12 @@ void testAllPathsFromSourceToTarget() {
 
 	vector<vector<int>> graph = { {1, 2},{3},{3},{} };
 	vector<vector<int>> output = Solution().allPathsSourceTarget(graph);
+
+	/*
+		Example 2:
+			Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+			Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+	*/
+	graph = { {4,3,1},{3,2,4},{3},{4}, {} };
+	output = Solution().allPathsSourceTarget(graph);
 }
