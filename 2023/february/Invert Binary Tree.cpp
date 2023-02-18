d1c996845067eee32084f9bc6fa39ea3350dd684
Pritam Nikam <pritam.nikam@gmail.com>
2023-02-18 12:00:45 +0530

    Create Invert Binary Tree.cpp

---
+++ 2023/february/Invert Binary Tree.cpp
@@ -0,0 +1,71 @@
+/*
+226. Invert Binary Tree
+https://leetcode.com/problems/invert-binary-tree/description/
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+
+namespace {
+    /**
+     * Definition for a binary tree node.
+     */
+    struct TreeNode {
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    };
+
+	class Solution {
+	public:
+		TreeNode* invertTree(TreeNode* root) {
+            if (root == nullptr) return root;
+
+            TreeNode* l = invertTree(root->left);
+            TreeNode* r = invertTree(root->right);
+
+            root->left = r;
+            root->right = l;
+            return root;
+		}
+	};
+
+}  // namespace
+
+void testInvertBinaryTree() {
+    TreeNode one(1), two(2), three(3);
+    two.left = &one; two.right = &three;
+    TreeNode* t = Solution().invertTree(&one);
+}
