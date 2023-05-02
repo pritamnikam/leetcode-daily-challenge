185f1096dd8f54642307824f9c392e6619d2e892
Pritam Nikam <pritam.nikam@gmail.com>
2023-05-02 07:48:55 +0530

    Create 02 Sign of the Product of an Array.cpp

---
+++ 2023/may/02 Sign of the Product of an Array.cpp
@@ -0,0 +1,68 @@
+/*
+1822. Sign of the Product of an Array
+https://leetcode.com/problems/sign-of-the-product-of-an-array/
+
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+Example 1:
+Input: nums = [-1,-2,-3,-4,3,2,1]
+Output: 1
+Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+
+Example 2:
+Input: nums = [1,5,0,2,-3]
+Output: 0
+Explanation: The product of all values in the array is 0, and signFunc(0) = 0
+
+Example 3:
+Input: nums = [-1,1,-1,1,-1]
+Output: -1
+Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
+
+Constraints:
+1 <= nums.length <= 1000
+-100 <= nums[i] <= 100
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+namespace {
+	class Solution {
+		int signFunc(int num) {
+			return (num == 0)
+				? 0
+				: (num > 0) ? 1 : -1;
+		}
+
+	public:
+		int arraySign(vector<int>& nums) {
+			int negative = 0;
+			for (int n : nums) {
+				int sign = signFunc(n);
+				if (sign == 0)
+					return 0;
+				else if (sign == -1)
+					negative++;
+			}
+
+			return (negative % 2) ? -1 : 1;
+		}
+	};
+}  // namespace
+
+void testSignOfTheProductOfAnArray() {
+	// Input: nums = [-1,-2,-3,-4,3,2,1], Output: 1
+	vector<int> nums = {-1, -2, -3, -4, 3, 2, 1};
+	cout << "Sign of the Product of an Array: " << Solution().arraySign(nums) << endl;
+}
