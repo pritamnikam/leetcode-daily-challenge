33651d139288090b5b7b32819162ce385bddfa2e
Pritam Nikam <pritam.nikam@gmail.com>
2023-06-14 07:36:31 +0530

    Create 14 Minimum Absolute Difference in BST.cpp

---
+++ 2023/june/14 Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,62 @@
+/*
+530. Minimum Absolute Difference in BST
+https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+Example 1:
+Input: root = [4,2,6,1,3]
+Output: 1
+Example 2:
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+Constraints:
+
+The number of nodes in the tree is in the range [2, 104].
+0 <= Node.val <= 105
+
+Note: This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+namespace {
+	/**
+	 * Definition for a binary tree node.
+	 */
+	struct TreeNode {
+		int val;
+		TreeNode* left;
+		TreeNode* right;
+		TreeNode() : val(0), left(nullptr), right(nullptr) {}
+		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+		TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+	};
+
+	class Solution {
+		vector<int> nums;
+
+		void PreOrder(TreeNode* root) {
+			if (root == nullptr) return;
+
+			PreOrder(root->left);
+			nums.push_back(root->val);
+			PreOrder(root->right);
+		}
+	public:
+		int getMinimumDifference(TreeNode* root) {
+			int result = INT_MAX;
+			PreOrder(root);
+
+			for (int i = 1; i < nums.size(); i++) {
+				result = min(result, nums[i] - nums[i - 1]);
+			}
+
+			return result;
+		}
+	};
+}  // namespace
