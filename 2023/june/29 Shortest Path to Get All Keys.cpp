81ce0376c2c1bb400a1cfdb559a783a56d828e75
Pritam Nikam <pritam.nikam@gmail.com>
2023-06-29 10:26:14 +0530

    Create 29 Shortest Path to Get All Keys.cpp

---
+++ 2023/june/29 Shortest Path to Get All Keys.cpp
@@ -0,0 +1,122 @@
+/*
+864. Shortest Path to Get All Keys
+https://leetcode.com/problems/shortest-path-to-get-all-keys/
+
+You are given an m x n grid grid where:
+
+'.' is an empty cell.
+'#' is a wall.
+'@' is the starting point.
+Lowercase letters represent keys.
+Uppercase letters represent locks.
+You start at the starting point and one move consists of walking one space in one of the four cardinal directions. You cannot walk outside the grid, or walk into a wall.
+
+If you walk over a key, you can pick it up and you cannot walk over a lock unless you have its corresponding key.
+
+For some 1 <= k <= 6, there is exactly one lowercase and one uppercase letter of the first k letters of the English alphabet in the grid. This means that there is exactly one key for each lock, and one lock for each key; and also that the letters used to represent the keys and locks were chosen in the same order as the English alphabet.
+
+Return the lowest number of moves to acquire all keys. If it is impossible, return -1.
+
+Example 1:
+Input: grid = ["@.a..","###.#","b.A.B"]
+Output: 8
+Explanation: Note that the goal is to obtain all the keys not to open all the locks.
+
+Example 2:
+Input: grid = ["@..aA","..B#.","....b"]
+Output: 6
+
+Example 3:
+Input: grid = ["@Aa"]
+Output: -1
+
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 30
+grid[i][j] is either an English letter, '.', '#', or '@'.
+The number of keys in the grid is in the range [1, 6].
+Each key in the grid is unique.
+Each key in the grid has a matching lock.
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+namespace {
+    class Solution {
+    public:
+
+        int dir[4][2] = { {0, 1}, {0, -1}, {-1, 0}, {1, 0} };
+
+        int shortestPathAllKeys(vector<string>& grid) {
+
+            int m = grid.size();
+            int n = grid[0].size();
+            queue<pair<pair<int, int>, int>> que;
+            vector<vector<unsigned long>> mapKey(m, vector<unsigned long>(n, 0));
+            int target = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if ('a' <= grid[i][j] && grid[i][j] <= 'f') {
+                        target |= 1 << (grid[i][j] - 'a');
+                        continue;
+                    }
+
+                    if (grid[i][j] == '@') {
+                        que.emplace(pair<int, int>(i, j), 0);
+                        mapKey[i][j] = 1;
+                    }
+                }
+            }
+
+
+            int step = 0;
+            while (!que.empty()) {
+                int size = que.size();
+                for (int s = 0; s < size; s++) {
+                    int i = que.front().first.first;
+                    int j = que.front().first.second;
+                    int key = que.front().second;
+                    que.pop();
+
+                    if ('a' <= grid[i][j] && grid[i][j] <= 'f') {
+                        key |= 1 << (grid[i][j] - 'a');
+                        if (key == target)
+                            return step;
+
+                        mapKey[i][j] |= 1l << key;
+                    }
+
+                    for (int d = 0; d < 4; d++) {
+                        int newi = i + dir[d][0];
+                        int newj = j + dir[d][1];
+
+                        if (!(0 <= newi && newi < m && 0 <= newj && newj < n))
+                            continue;
+
+                        int val = grid[newi][newj];
+                        if (val == '#')
+                            continue;
+
+                        if ('A' <= val && val <= 'F' && (key & (1 << (val - 'A'))) == 0)
+                            continue;
+
+                        if (mapKey[newi][newj] & (1l << key))
+                            continue;
+
+                        mapKey[newi][newj] |= 1l << key;
+                        que.emplace(pair<int, int>(newi, newj), key);
+                    }
+                }
+                step++;
+            }
+            return -1;
+        }
+    };
+}
