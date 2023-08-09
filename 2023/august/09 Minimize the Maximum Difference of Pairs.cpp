bf7a118ff57d07c364d35a8ae7f8cddea13768bd
Pritam Nikam <pritam.nikam@gmail.com>
2023-08-09 08:59:30 +0530

    Create 09 Minimize the Maximum Difference of Pairs.cpp

---
+++ 2023/august/09 Minimize the Maximum Difference of Pairs.cpp
@@ -0,0 +1,15 @@
+/*
+2616. Minimize the Maximum Difference of Pairs
+https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/
+
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace {
+} // namespace
