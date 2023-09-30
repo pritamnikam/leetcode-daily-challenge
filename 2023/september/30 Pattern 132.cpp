ae0c8eb873fb89011ad1dd8df64e655cd1f9047d
Pritam Nikam <pritam.nikam@gmail.com>
2023-09-30 19:59:39 +0530

    Update 30 Pattern 132.cpp

--- 2023/september/30 Pattern 132.cpp
+++ 2023/september/30 Pattern 132.cpp
@@ -1,7 +1,7 @@
 // 456. Pattern 132
 // https://leetcode.com/problems/132-pattern/description/
 
-using namespace tsd;
+using namespace std;
 
 #include <iostream>
 #include <vector>
@@ -11,6 +11,68 @@ namespace {
 
 class Solution {
 public:
+    // Bruit Force: O(n^3)
+    bool find132PatternBruitForce(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 2; i++) {
+            for (int j = i + 1; j < nums.size() - 1; j++) {
+                for (int k = j + 1; k < nums.size(); k++) {
+                    if (nums[k] > nums[i] && nums[j] > nums[k]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    // Bruit Force Improvement: O(n^2)
+    bool find132PatternTwoForloops(vector<int>& nums) {
+        int min_i = INT_MAX;
+        for (int j = 0; j < nums.size() - 1; j++) {
+            min_i = min(min_i, nums[j]);
+            for (int k = j + 1; k < nums.size(); k++) {
+                if (nums[k] < nums[j] && min_i < nums[k]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    // Bruit Force Improvement: O(n)
+    bool find132PatternOneLoop() {
+        int length = nums.size();
+
+        // Check if the array has less than 3 elements
+        if (length < 3)
+            return false;
+
+        // Stack to keep track of decreasing elements.
+        stack<int> decreasingStack;
+
+        // Maximum value of the third element in the 132 pattern.
+        int maxThirdElement = INT_MIN;
+
+        // Traverse the array from right to left
+        for (int i = length - 1; i >= 0; i--) {
+            int currentNumber = nums[i];
+
+            // Check for 132 pattern
+            if (currentNumber < maxThirdElement)
+                return true;  // Found a 132 pattern.
+
+            // Maintain the stack with decreasing elements
+            while (!decreasingStack.empty() && decreasingStack.top() < currentNumber) {
+                maxThirdElement = decreasingStack.top();
+                decreasingStack.pop();
+            }
+
+            // Push the current element onto the stack.
+            decreasingStack.push(currentNumber);
+        }
+
+        return false; // No 132 pattern found
+    }
+
     bool find132pattern(vector<int>& nums) {
       int n = nums.size();
       if (n < 3) return false;
