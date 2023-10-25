bb5f64ab2076937dde5f7b9c0d2870a5aef269b7
Pritam Nikam <pritam.nikam@gmail.com>
2023-10-25 08:01:33 +0530

    Update 25 K-th Symbol in Grammer.cpp

--- 2023/october/25 K-th Symbol in Grammer.cpp
+++ 2023/october/25 K-th Symbol in Grammer.cpp
@@ -14,6 +14,18 @@ public:
         int count = __builtin_popcount(k - 1);
         return (count % 2 == 0) ? 0 : 1;
     }
+private:
+    int dfs(int n, int k, int rootVal) {
+        if (n == 1) return rootVal;
+        int totalNodes = pow(2, n-1);
+        if (k > totalNodes/2) {
+            int nextRootVal = (rootVal == 0) ? 1 : 0;
+            return dfs(n - 1, k - (totalNodes / 2), nextRootVal);
+        } else {
+            int nextRootVal = (rootVal == 0) ? 0 : 1;
+            return dfs(n-1, k, nextRootVal);
+        }
+    }
 };
 
 }
