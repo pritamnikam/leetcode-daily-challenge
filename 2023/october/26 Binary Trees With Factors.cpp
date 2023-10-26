3417a57c5bdf29cb52bbaf324d028e0972f8247f
Pritam Nikam <pritam.nikam@gmail.com>
2023-10-26 07:40:01 +0530

    Create 26 Binary Trees With Factors.cpp

---
+++ 2023/october/26 Binary Trees With Factors.cpp
@@ -0,0 +1,45 @@
+// 823. Binary Trees With Factors
+// https://leetcode.com/problems/binary-trees-with-factors
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+namespace {
+class Solution {
+private:
+    const int mod = 1e9 + 7;
+public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+
+        vector<long> dp(n);
+        dp[0] = 1;
+        int res = 0;
+
+        for (int i = 1; i < n; i++) {
+            int target = arr[i];
+            int p = 0, q = i - 1;
+            long ways = 1;
+
+            while (p <= q) {
+                long mul = ((long) arr[p]) * arr[q];
+                if (mul == target) {
+                    if (p == q) ways += (dp[p] * dp[q]) % mod;
+                    else ways += (dp[p] * dp[q] * 2) % mod;
+                    ++p; q--;
+                }
+                else if (mul < target) p++;
+                else if (mul > target) q--;
+            }
+            dp[i] = ways;
+            res = (res + dp[i]) % mod;
+        }
+
+        return res + 1;
+    }
+};
+}
