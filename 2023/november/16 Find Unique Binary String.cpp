d1fb007650f218307f7bf5cc54cb75afe65574a9
Pritam Nikam <pritam.nikam@gmail.com>
2023-11-16 11:53:32 +0530

    Create 16 Find Unique Binary String.cpp

---
+++ 2023/november/16 Find Unique Binary String.cpp
@@ -0,0 +1,34 @@
+// 1980. Find Unique Binary String
+// https://leetcode.com/problems/find-unique-binary-string
+
+class Solution {
+public:
+    int n;
+    unordered_set<string> numsSet;
+    
+    string generate(string curr) {
+        if (curr.size() == n) {
+            if (numsSet.find(curr) == numsSet.end()) {
+                return curr;
+            }
+            
+            return "";
+        }
+        
+        string addZero = generate(curr + "0");
+        if (addZero.size() > 0) {
+            return addZero;
+        }
+        
+        return generate(curr + "1");
+    }
+    
+    string findDifferentBinaryString(vector<string>& nums) {
+        n = nums.size();
+        for (string s : nums) {
+            numsSet.insert(s);
+        }
+        
+        return generate("");
+    }
+};
