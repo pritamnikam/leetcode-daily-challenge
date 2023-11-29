031832e3397684f70db0df0cdef72dd844e9b088
Pritam Nikam <pritam.nikam@gmail.com>
2023-11-29 21:19:33 +0530

    Create 29 Number of 1 Bits.cpp

---
+++ 2023/november/29 Number of 1 Bits.cpp
@@ -0,0 +1,15 @@
+// 191. Number of 1 Bits
+// https://leetcode.com/problems/number-of-1-bits/description/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n-1);   
+            count++;
+        }
+        
+        return count;
+    }
+};
