f11a96ac6bfa7694f687f782d26191f102fd092c
Pritam Nikam <pritam.nikam@gmail.com>
2024-01-01 08:31:18 +0530

    Create 01 Assign Cookies.cpp

---
+++ 2024/january/01 Assign Cookies.cpp
@@ -0,0 +1,21 @@
+// 455. Assign Cookies
+// https://leetcode.com/problems/assign-cookies/
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int contentChildren = 0;
+        int cookieIndex = 0;
+
+        while (cookieIndex < s.size() && contentChildren < g.size()) {
+            if (s[cookieIndex] >= g[contentChildren])
+                ++contentChildren;
+            ++cookieIndex;
+        }
+
+        return contentChildren;
+    }
+};
