c601d61dd934a336f1b2ed1cf23dcc1931a88163
Pritam Nikam <pritam.nikam@gmail.com>
2024-01-05 22:26:33 +0530

    Create 05 Longest Increasing Subsequence.cpp

---
+++ 2024/january/05 Longest Increasing Subsequence.cpp
@@ -0,0 +1,22 @@
+// 300. Longest Incresing Subsequence
+// https://leetcode.com/problems/longest-increasing-subsequence
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        
+        int maxLength = 1;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j] && dp[i] <= dp[j]) {
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                    maxLength = max(maxLength, dp[i]);
+                }
+            }
+        }
+        
+        return maxLength;
+    }
+};
