4222fb9cebb8c4ecdc589464b41482718bee7ed5
Pritam Nikam <pritam.nikam@gmail.com>
2024-02-19 21:37:52 +0530

    Create 19 Power of Two.cpp

---
+++ 2024/february/19 Power of Two.cpp
@@ -0,0 +1,11 @@
+// 231. Power of Two
+// https://leetcode.com/problems/power-of-two
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return (n < 0)
+          ? false
+          : (n & (n - 1)) == 0;
+    }
+};
