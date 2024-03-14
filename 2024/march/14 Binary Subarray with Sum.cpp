b1c1ec3f121db0772f591816d78c9ef1f2c6ae01
Pritam Nikam <pritam.nikam@gmail.com>
2024-03-14 10:35:17 +0530

    Create 14 Binary Subarray with Sum.cpp

---
+++ 2024/march/14 Binary Subarray with Sum.cpp
@@ -0,0 +1,31 @@
+// 930. Binary Subarray with Sum
+// https://leetcode.com/problems/binary-subarrays-with-sum/
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return slidingWindowAtMost(nums, goal)
+            - slidingWindowAtMost(nums, goal - 1);
+    }
+
+    private:
+    int slidingWindowAtMost(
+        vector<int>& nums,
+        int goal
+    ) {
+        int start = 0, currentSum = 0, totalCount = 0;
+
+        for (int end = 0; end < nums.size(); end++) {
+            currentSum += nums[end];
+
+            while (start <= end && currentSum > goal) {
+                currentSum -= nums[start];
+                start++;
+            }
+
+            totalCount += end - start + 1;
+        }
+
+        return totalCount;
+    }
+};
