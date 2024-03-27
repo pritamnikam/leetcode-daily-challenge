c2bf014b879abb5e3f154da1b5336e59cf7981f1
Pritam Nikam <pritam.nikam@gmail.com>
2024-03-27 22:39:41 +0530

    Create 27 Subarray Product Less Than K.cpp

---
+++ 2024/march/27 Subarray Product Less Than K.cpp
@@ -0,0 +1,26 @@
+// 713. Subarray Product Less Than K
+// https://leetcode.com/problems/subarray-product-less-than-k
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+
+        int totalCount = 0;
+        int product = 1;
+
+        for (int left = 0, right = 0; right < nums.size(); right++) {
+            product *= nums[right];
+
+            while (product >= k) {
+                product /= nums[left++];
+            }
+
+            totalCount += right - left + 1;
+        }
+
+        return totalCount;
+    }
+};
