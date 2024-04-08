dcb90d6bfb0a47d673473b9d71cfc73795d6c6c8
Pritam Nikam <pritam.nikam@gmail.com>
2024-04-08 10:10:46 +0530

    Create 08 Number of Students Unable to Eat Lunch.cpp

---
+++ 2024/april/08 Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,39 @@
+// 1700. Number of Students Unable to Eat Lunch
+// https://leetcode.com/problems/number-of-students-unable-to-eat-lunch
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int circleStudentCount = 0;
+        int squareStudentCount = 0;
+
+        // Count the number of students who want each type of sandwich
+        for (int student : students) {
+            if (student == 0) {
+                circleStudentCount++;
+            } else {
+                squareStudentCount++;
+            }
+        }
+
+        // Serve sandwiches to students
+        for (int sandwich : sandwiches) {
+            // No student wants the circle sandwich on top of the stack
+            if (sandwich == 0 && circleStudentCount == 0) {
+                return squareStudentCount;
+            } 
+            // No student wants the square sandwich on top of the stack
+            if (sandwich == 1 && squareStudentCount == 0) {
+                return circleStudentCount;
+            }
+            // Decrement the count of the served sandwich type
+            if (sandwich == 0) {
+                circleStudentCount--;
+            } else {
+                squareStudentCount--;
+            }
+        }
+        // Every student received a sandwich
+        return 0;
+    }
+};
