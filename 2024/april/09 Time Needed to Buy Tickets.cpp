b0f109c9bc4c63108a471c6ac82fd714f43a7592
Pritam Nikam <pritam.nikam@gmail.com>
2024-04-09 14:28:50 +0530

    Create 09 Time Needed to Buy Tickets.cpp

---
+++ 2024/april/09 Time Needed to Buy Tickets.cpp
@@ -0,0 +1,17 @@
+// 2073. Time Needed to Buy Tickets
+// https://leetcode.com/problems/time-needed-to-buy-tickets/
+
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        for (int i = 0; i < tickets.size(); i++) {
+            if (i <= k) {
+                time += min(tickets[k], tickets[i]);
+            } else {
+                time += min(tickets[k] - 1, tickets[i]);
+            }
+        }
+        return time;
+    }
+};
