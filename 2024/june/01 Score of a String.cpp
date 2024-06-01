43324d7e00f89a7138caefc8f6f30cb5ae90a52e
Pritam Nikam <pritam.nikam@gmail.com>
2024-06-01 12:23:07 +0530

    Create 01 Score of a String.cpp

---
+++ 2024/june/01 Score of a String.cpp
@@ -0,0 +1,13 @@
+// 3110. Score of a String
+// https://leetcode.com/problems/score-of-a-string/
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            score += abs(s[i] - s[i+1]);
+        }
+        return score;
+    }
+};
