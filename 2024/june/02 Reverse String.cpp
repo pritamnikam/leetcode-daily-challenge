66cd01099ca76c545136a272b8b5bef1bb380614
Pritam Nikam <pritam.nikam@gmail.com>
2024-06-02 17:13:10 +0530

    Create 02 Reverse String.cpp

---
+++ 2024/june/02 Reverse String.cpp
@@ -0,0 +1,12 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i = 0; i < s.size()/2; i++) {
+            // swap
+            swap(s[i], s[s.size() - 1 - i]);
+        }
+    }
+};
