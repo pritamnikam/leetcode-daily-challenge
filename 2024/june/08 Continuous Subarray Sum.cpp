ea47c3cfd9089bef21c2a7841ca1ea66c257e19f
Pritam Nikam <pritam.nikam@gmail.com>
2024-06-08 11:29:38 +0530

    Create 08 Continuous Subarray Sum.cpp

---
+++ 2024/june/08 Continuous Subarray Sum.cpp
@@ -0,0 +1,23 @@
+// 523. Continuous Subarray Sum
+// https://leetcode.com/problems/continuous-subarray-sum
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size(),
+            sum = 0,
+            pre = 0;
+
+        unordered_set<int> modk;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            int mod = (k == 0) ? sum : sum % k;
+            if (modk.count(mod)) {
+              return true;
+            }
+            modk.insert(pre);
+            pre = mod;
+        }
+        return false;
+    }
+};
