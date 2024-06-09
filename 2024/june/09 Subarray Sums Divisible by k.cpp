f8470523f9cec025f138e481c0677b92fe8b3a20
Pritam Nikam <pritam.nikam@gmail.com>
2024-06-09 17:05:55 +0530

    Create 09 Subarray Sums Divisible by k.cpp

---
+++ 2024/june/09 Subarray Sums Divisible by k.cpp
@@ -0,0 +1,26 @@
+// 974. Subarray Sums Divisible by k
+// https://leetcode.com/problems/subarray-sums-divisible-by-k/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int prefixMod = 0;
+        int result = 0;
+
+        vector<int> modGroups(k);
+        modGroups[0] = 1;
+
+        for (int num: nums) {
+            // Take modulo twice to avoid negative remainders.
+            prefixMod = (prefixMod + num % k + k) % k;
+
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            result += modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return result;
+    }
+};
