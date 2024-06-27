f8394dcc0196b2fe1d850e050c1b57806111d328
Pritam Nikam <pritam.nikam@gmail.com>
2024-06-27 10:14:52 +0530

    Create 27 Find Center of Star Graph.cpp

---
+++ 2024/june/27 Find Center of Star Graph.cpp
@@ -0,0 +1,14 @@
+// 1791. Find Center of Star Graph
+// https://leetcode.com/problems/find-center-of-star-graph/
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        vector<int> firstEdge = edges[0];
+        vector<int> secondEdge = edges[1];
+
+        return (firstEdge[0] == secondEdge[0] || firstEdge[0] == secondEdge[1])
+                   ? firstEdge[0]
+                   : firstEdge[1];
+    }
+};
