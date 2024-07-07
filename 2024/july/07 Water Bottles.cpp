b7f054ef096431d735238109683be42b32b9895e
Pritam Nikam <pritam.nikam@gmail.com>
2024-07-07 11:32:49 +0530

    Create 07 Water Bottles.cpp

---
+++ 2024/july/07 Water Bottles.cpp
@@ -0,0 +1,17 @@
+// 1518. Water Bottles
+// 
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int consumedBottles = 0;
+        while (numBottles >= numExchange) {
+            int k = numBottles / numExchange;
+            consumedBottles += numExchange * k;
+            numBottles -= numExchange * k;
+            numBottles += k;
+        }
+
+        return consumedBottles + numBottles;
+    }
+};
