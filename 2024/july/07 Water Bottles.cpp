0dd7484ff50844fcdf54de0820ba4aad5b5f32c4
Pritam Nikam <pritam.nikam@gmail.com>
2024-07-07 11:39:37 +0530

    Update 07 Water Bottles.cpp

--- 2024/july/07 Water Bottles.cpp
+++ 2024/july/07 Water Bottles.cpp
@@ -1,14 +1,21 @@
 // 1518. Water Bottles
-// 
+// https://leetcode.com/problems/water-bottles/
 
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int consumedBottles = 0;
         while (numBottles >= numExchange) {
+            // Bottles will be exchanged in the current round
             int k = numBottles / numExchange;
+
+            // cumulative sum of bottles consumed so far after this round
             consumedBottles += numExchange * k;
+
+            // remaining filled bottles post this round
             numBottles -= numExchange * k;
+
+            // new set of filled bottles after this round
             numBottles += k;
         }
 
