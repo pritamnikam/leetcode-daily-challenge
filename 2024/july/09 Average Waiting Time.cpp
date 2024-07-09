2881dea84b0b04d1982a6f94dd2582322ca8aed5
Pritam Nikam <pritam.nikam@gmail.com>
2024-07-09 20:25:16 +0530

    Create 09 Average Waiting Time.cpp

---
+++ 2024/july/09 Average Waiting Time.cpp
@@ -0,0 +1,23 @@
+// 1701. Average Waiting Time
+// https://leetcode.com/problems/average-waiting-time
+
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int nextIdleTime = 0;
+        long long netWaitTime = 0;
+
+        for (int i = 0; i < customers.size(); i++) {
+            // The next idle time for the chef is given by the time of delivery
+            // of current customer's order.
+            nextIdleTime = max(customers[i][0], nextIdleTime) + customers[i][1];
+            
+            // The wait time for the current customer is the difference between
+            // his delivery time and arrival time.            
+            netWaitTime += nextIdleTime - customers[i][0];
+        }
+        // Divide by total customers to get average.
+        double avgWaitTime = static_cast<double>(netWaitTime) / customers.size();
+        return avgWaitTime;
+    }
+};
