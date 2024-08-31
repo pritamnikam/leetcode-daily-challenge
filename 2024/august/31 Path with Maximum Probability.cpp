4a7f086b07a12961af70479992fe4fcf3c5abad2
Pritam Nikam <pritam.nikam@gmail.com>
2024-08-31 12:40:55 +0530

    Create 31 Path with Maximum Probability.cpp

---
+++ 2024/august/31 Path with Maximum Probability.cpp
@@ -0,0 +1,43 @@
+// 1514. Path with Maximum Probability
+// https://leetcode.com/problems/path-with-maximum-probability
+
+class Solution {
+public:
+    double maxProbability(
+        int n, 
+        vector<vector<int>>& edges, 
+        vector<double>& succProb, 
+        int start, 
+        int end
+    ) {
+        vector<double> maxProb = vector<double>(n, 0);
+        maxProb[start] = 1.0;
+
+        // Bellmon-Ford Shortest Path
+        for (int i = 0; i < n - 1; i++) {
+            bool hasUpdated = false;
+
+            // Relax all edges
+            for (int j = 0; j < edges.size(); j++) {
+                int u = edges[j][0],
+                    v = edges[j][1];
+                double pathProb = succProb[j];
+
+                if (maxProb[u] * pathProb > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * pathProb;
+                    hasUpdated = true;
+                }
+                if (maxProb[v] * pathProb > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * pathProb;
+                    hasUpdated = true;
+                }
+            }
+
+            if (!hasUpdated) {
+                break;
+            }
+        }
+
+        return maxProb[end];
+    }
+};
