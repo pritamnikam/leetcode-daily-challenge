73f6f78e9c1a7076f36377012ec51d5b0ca83a95
Pritam Nikam <pritam.nikam@gmail.com>
2024-09-05 22:23:43 +0530

    Create 05 Find Missing Observations.cpp

---
+++ 2024/september/05 Find Missing Observations.cpp
@@ -0,0 +1,26 @@
+// 2028. Find Missing Observations
+// https://leetcode.com/problems/find-missing-observations/
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int sum = 0;
+        for (int i = 0; i < rolls.size(); i++) {
+            sum = sum + rolls[i];
+        }
+        // Find the remaining sum.
+        int remainingSum = mean * (n + rolls.size()) - sum;
+        // Check if sum is valid or not.
+        if (remainingSum > 6 * n or remainingSum < n) {
+            return {};
+        }
+        int distributeMean = remainingSum / n;
+        int mod = remainingSum % n;
+        // Distribute the remaining mod elements in nElements array.
+        vector<int> nElements(n, distributeMean);
+        for (int i = 0; i < mod; i++) {
+            nElements[i]++;
+        }
+        return nElements;
+    }
+};
