a88e41be347cc5e80b3c2f92a806fd8c56801cb2
Pritam Nikam <pritam.nikam@gmail.com>
2024-09-06 08:54:06 +0530

    Create 06 Delete Nodes from Linked list Present in an Array.cpp

---
+++ 2024/september/06 Delete Nodes from Linked list Present in an Array.cpp
@@ -0,0 +1,42 @@
+//
+// 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> valuesToRemove(nums.begin(), nums.end());
+
+        while (head != nullptr && valuesToRemove.count(head->val)) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+        }
+
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        ListNode* current = head;
+        while (current->next != nullptr) {
+            if (valuesToRemove.contains(current->next->val)) {
+                ListNode* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+            }else {
+                // Move to the next node
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
