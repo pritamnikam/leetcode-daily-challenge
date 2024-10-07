79adb1f6852a38fa27fcd7ecb22f60fa54513a6c
Pritam Nikam <pritam.nikam@gmail.com>
2024-10-07 09:08:08 +0530

    Create 07 Minimum String Length After Removing Substrings.cpp

---
+++ 2024/october/07 Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,29 @@
+// 2696. Minimum String Length After Removing Substrings
+// https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+
+class Solution {
+public:
+    int minLength(string s) {
+        int writePtr = 0;
+        vector<char> charArray(s.begin(), s.end());
+
+        for (int readPtr = 0; readPtr < s.length(); readPtr++) {
+            charArray[writePtr] = charArray[readPtr];
+
+            if (
+                writePtr > 0 &&
+                (
+                    charArray[writePtr - 1] == 'A'||
+                    charArray[writePtr - 1] == 'C'
+                ) &&
+                charArray[writePtr] == charArray[writePtr - 1] + 1
+            ) {
+                writePtr--;
+            } else {
+                writePtr++;
+            }
+        }
+
+        return writePtr;
+    }
+};
