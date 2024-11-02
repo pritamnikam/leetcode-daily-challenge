abdf4cd821e2a0e7428b0cbadd3840a3318b869c
Pritam Nikam <pritam.nikam@gmail.com>
2024-11-02 12:51:13 +0530

    Create 02 Circular Sentence.cpp

---
+++ 2024/november/02 Circular Sentence.cpp
@@ -0,0 +1,15 @@
+// 2490. Circular Sentence
+// https://leetcode.com/problems/circular-sentence/
+
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        for (int i = 0; i< sentence.size(); i++) {
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1]) {
+                return false;
+            }
+        }
+
+        return sentence[0] == sentence[sentence.size() - 1];
+    }
+};
