3a5d1087703b4348034c0119346bd7138c7e08a4
Pritam Nikam <pritam.nikam@gmail.com>
2024-11-13 08:36:35 +0530

    Create 13 Count the Number of Fair Pairs.cpp

---
+++ 2024/november/13 Count the Number of Fair Pairs.cpp
@@ -0,0 +1,30 @@
+// 2563. Count the Number of Fair Pairs
+// https://leetcode.com/problems/count-the-number-of-fair-pairs/
+
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+    }
+
+private:
+    long long lower_bound(vector<int>& nums, int value) {
+        int left = 0, right = nums.size() - 1;
+        long long result = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            // If sum is less than value, add the size of window to result and
+            // move to the next index.
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else {
+                // Otherwise, shift the right pointer backwards, until we get a
+                // valid window.
+                right--;
+            }
+        }
+        return result;
+    }
+};
