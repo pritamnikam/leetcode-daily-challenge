80966169e7c0447e36b8b4b8b953fe299551d217
Pritam Nikam <pritam.nikam@gmail.com>
2024-11-16 13:42:54 +0530

    Create 16 find-the-power-of-k-size-subarrays-i.cpp

---
+++ 2024/november/16 find-the-power-of-k-size-subarrays-i.cpp
@@ -0,0 +1,34 @@
+// find-the-power-of-k-size-subarrays-i
+// https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int length = nums.size();
+        vector<int> result(length - k + 1, -1);
+        deque<int> indexDeque;
+
+        for (int currentIndex = 0; currentIndex < length; currentIndex++) {
+            if (!indexDeque.empty() &&
+                indexDeque.front() < currentIndex - k + 1) {
+                indexDeque.pop_front();
+            }
+
+            if (!indexDeque.empty() &&
+                nums[currentIndex] != nums[currentIndex - 1] + 1) {
+                indexDeque.clear();
+            }
+
+            indexDeque.push_back(currentIndex);
+
+            if (currentIndex >= k - 1) {
+                if (indexDeque.size() == k) {
+                    result[currentIndex - k + 1] = nums[indexDeque.back()];
+                } else {
+                    result[currentIndex - k + 1] = -1;
+                }
+            }
+        }
+
+        return result;
+    }
+};
