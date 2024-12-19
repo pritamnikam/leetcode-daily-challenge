5182363e8aee68297ef252f180b07ef23bbc78a1
Pritam Nikam <pritam.nikam@gmail.com>
2024-12-19 22:08:18 +0530

    Create 19 max-chunks-to-make-sorted.cpp

---
+++ 2024/december/19 max-chunks-to-make-sorted.cpp
@@ -0,0 +1,19 @@
+// 769. max-chunks-to-make-sorted
+// https://leetcode.com/problems/max-chunks-to-make-sorted
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        int chunks = 0, prefixSum = 0, sortedPrefixSum = 0;
+
+        for (int i = 0;i < n;i++) {
+            prefixSum += arr[i];
+            sortedPrefixSum += i;
+            if (prefixSum == sortedPrefixSum) {
+                chunks++;
+            }
+        }
+        return chunks;
+    }
+};
