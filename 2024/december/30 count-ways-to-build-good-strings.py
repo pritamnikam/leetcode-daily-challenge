143044f66fc61c7cc2704daf8dfbc7f9debdd4b9
Pritam Nikam <pritam.nikam@gmail.com>
2024-12-30 15:28:08 +0530

    Create 30 count-ways-to-build-good-strings.py

---
+++ 2024/december/30 count-ways-to-build-good-strings.py
@@ -0,0 +1,20 @@
+# 2466. count-ways-to-build-good-strings
+# https://leetcode.com/problems/count-ways-to-build-good-strings/
+
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        dp = [1] + [-1] * (high)
+        mod = 10 ** 9 + 7
+
+        def dfs(end):
+            if dp[end] != -1:
+                return dp[end]
+            count = 0
+            if end >= zero:
+                count += dfs(end - zero)
+            if end >= one:
+                count += dfs(end - one)
+            dp[end] = count % mod
+            return dp[end]
+        
+        return sum(dfs(end) for end in range(low, high + 1)) % mod
