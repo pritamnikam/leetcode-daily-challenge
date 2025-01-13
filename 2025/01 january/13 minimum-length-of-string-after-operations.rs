73102d3d02dacdc053f689ae65472acfe111fb05
Pritam Nikam <pritam.nikam@gmail.com>
2025-01-13 10:39:00 +0530

    Create 13 minimum-length-of-string-after-operations.rs

---
+++ 2025/01 january/13 minimum-length-of-string-after-operations.rs
@@ -0,0 +1,22 @@
+// 3223. minimum-length-of-string-after-operations
+// https://leetcode.com/problems/minimum-length-of-string-after-operations/
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn minimum_length(s: String) -> i32 {
+        let s: Vec<char> = s.chars().collect();
+        let mut d: HashMap<char, usize> = HashMap::with_capacity(26);
+
+        for c in s.iter() {
+            *d.entry(*c).or_insert(0) += 1;
+        }
+
+        let mut deleted: usize = 0;
+        for (k, v) in d.iter() {
+            deleted += if v & 1 == 0 {v-2} else {v-1};
+        }
+
+        (s.len() - deleted) as i32
+    }
+}
