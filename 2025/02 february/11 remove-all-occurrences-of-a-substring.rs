8917d51eaa3ca99a1722adcf29a20d8a136ccdf2
Pritam Nikam <pritam.nikam@gmail.com>
2025-02-11 09:23:37 +0530

    Create 11 remove-all-occurrences-of-a-substring.rs

---
+++ 2025/02 february/11 remove-all-occurrences-of-a-substring.rs
@@ -0,0 +1,12 @@
+// 1910. remove-all-occurrences-of-a-substring
+// https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+impl Solution {
+    pub fn remove_occurrences(s: String, part: String) -> String {
+        let mut result = s.into_bytes();
+        while let Some(p) = result.windows(part.len()).position(|win| win == part.as_bytes()) {
+            result.drain(p..part.len() + p);
+        }
+        result.into_iter().map(|b| b as char).collect()
+    }
+}
