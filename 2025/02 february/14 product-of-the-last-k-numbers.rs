d677da53cb1bf9f8b1c5672e367ede1c0e381329
Pritam Nikam <pritam.nikam@gmail.com>
2025-02-14 06:08:36 +0530

    Update 14 product-of-the-last-k-numbers.rs

--- 2025/02 february/14 product-of-the-last-k-numbers.rs
+++ 2025/02 february/14 product-of-the-last-k-numbers.rs
@@ -31,3 +31,43 @@ impl ProductOfNumbers {
  * obj.add(num);
  * let ret_2: i32 = obj.get_product(k);
  */
+use std::collections::VecDeque;
+
+struct ProductOfNumbers {
+    deq: VecDeque<i32>
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl ProductOfNumbers {
+
+    fn new() -> Self {
+        Self { deq: VecDeque::from([1]) }
+    }
+    
+    fn add(&mut self, num: i32) {
+        if num == 0 {
+            self.deq = VecDeque::from([1]);
+        } else {
+            self.deq.push_front(self.deq[0] * num);
+        }
+    }
+    
+    fn get_product(&self, k: i32) -> i32 {
+        if k as usize >= self.deq.len() {
+            0
+        } else {
+            self.deq[0] / self.deq[k as usize]
+        }
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * let obj = ProductOfNumbers::new();
+ * obj.add(num);
+ * let ret_2: i32 = obj.get_product(k);
+ */
