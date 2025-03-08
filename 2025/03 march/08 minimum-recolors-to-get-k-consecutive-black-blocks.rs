e31e4c3051662d49fafb626228b68526f184ef77
Pritam Nikam <pritam.nikam@gmail.com>
2025-03-08 20:37:29 +0530

    Create 08 minimum-recolors-to-get-k-consecutive-black-blocks.rs

---
+++ 2025/03 march/08 minimum-recolors-to-get-k-consecutive-black-blocks.rs
@@ -0,0 +1,23 @@
+// 2379. minimum-recolors-to-get-k-consecutive-black-blocks
+// https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks
+
+impl Solution {
+    pub fn minimum_recolors(blocks: String, k: i32) -> i32 {
+        let mut black_count = 0;
+        let mut ans = i32::MAX;
+        let k = k as usize;
+        let blocks: Vec<char> = blocks.chars().collect();
+
+        for i in 0..blocks.len() {
+            if i >= k && blocks[i - k] == 'B' {
+                black_count -= 1;
+            }
+            if blocks[i] == 'B' {
+                black_count += 1;
+            }
+            ans = ans.min(k as i32 - black_count);
+        }
+
+        ans
+    }
+}
