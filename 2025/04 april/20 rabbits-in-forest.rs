435f3f5cc9bad6e78d32602a0058ccc101cba732
Pritam Nikam <pritam.nikam@gmail.com>
2025-04-20 20:39:10 +0530

    Create 20 rabbits-in-forest.rs

---
+++ 2025/04 april/20 rabbits-in-forest.rs
@@ -0,0 +1,15 @@
+// rabbits-in-forest
+// https://leetcode.com/problems/rabbits-in-forest
+
+impl Solution {
+    pub fn num_rabbits(mut answers: Vec<i32>) -> i32 {
+        answers.sort_unstable();
+        answers.chunk_by(
+            |a, b| a == b
+            ).fold(
+                0, |sum, chunk| {
+                    let v = chunk[0] + 1;
+                    sum + (chunk[0] + chunk.len() as i32) / v * v
+            })
+    }
+}
