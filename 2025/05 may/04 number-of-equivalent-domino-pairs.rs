3b670a5b058f6788e4e8f985c2b9540c4ab907e1
Pritam Nikam <pritam.nikam@gmail.com>
2025-05-04 09:26:29 +0530

    Create 04 number-of-equivalent-domino-pairs.rs

---
+++ 2025/05 may/04 number-of-equivalent-domino-pairs.rs
@@ -0,0 +1,16 @@
+// 1128. number-of-equivalent-domino-pairs
+// https://leetcode.com/problems/number-of-equivalent-domino-pairs
+
+impl Solution {
+    pub fn num_equiv_domino_pairs(dominoes: Vec<Vec<i32>>) -> i32 {
+        dominoes.into_iter()
+            .map(|d| [d[0].min(d[1]), d[0].max(d[1])])
+            .fold(std::collections::HashMap::new(), |mut map, d| {
+                *map.entry(d).or_default() += 1;
+                map
+            })
+            .into_values()
+            .map(|c: i32| (c * (c - 1)) >> 1)
+            .sum()
+    }
+}
