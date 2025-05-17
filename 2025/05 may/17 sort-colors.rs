a6503140300afd8df5285db33b14c6dc0a50ed4d
Pritam Nikam <pritam.nikam@gmail.com>
2025-05-17 08:44:09 +0530

    Create 17 sort-colors.rs

---
+++ 2025/05 may/17 sort-colors.rs
@@ -0,0 +1,17 @@
+// 75. sort-colors
+// https://leetcode.com/problems/sort-colors
+
+impl Solution {
+    pub fn sort_colors(nums: &mut Vec<i32>) {
+        nums.iter()
+		.fold([0; 3], |mut f, c| {
+			f[*c as usize] += 1;
+			f
+		})
+		.into_iter()
+		.zip(0..)
+		.flat_map(|(cc, c)| std::iter::repeat_n(c, cc))
+		.zip(nums.iter_mut())
+		.for_each(|(nc, oc)| *oc = nc);
+    }
+}
