f5843e941d8acd38910cc488e8d765ca6246be89
Pritam Nikam <pritam.nikam@gmail.com>
2025-05-19 20:28:43 +0530

    Create 19 type-of-triangle.rs

---
+++ 2025/05 may/19 type-of-triangle.rs
@@ -0,0 +1,17 @@
+// 3024. type-of-triangle
+// https://leetcode.com/problems/type-of-triangle
+
+impl Solution {
+    pub fn triangle_type(mut nums: Vec<i32>) -> String {
+        nums.sort();
+        if nums[0] + nums[1] <= nums[2] {
+            "none".to_string()
+        } else if nums[0] == nums[2] {
+            "equilateral".to_string()
+        } else if nums[0] == nums[1] || nums[1] == nums[2] {
+            "isosceles".to_string()
+        } else {
+            "scalene".to_string()
+        }
+    }
+}
