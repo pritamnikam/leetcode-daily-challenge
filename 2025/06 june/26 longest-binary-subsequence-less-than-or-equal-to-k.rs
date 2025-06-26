5c8179b281190ae61e0153da51bd83d09ed67fd2
Pritam Nikam <pritam.nikam@gmail.com>
2025-06-26 16:08:11 +0530

    Create 26 longest-binary-subsequence-less-than-or-equal-to-k.rs

---
+++ 2025/06 june/26 longest-binary-subsequence-less-than-or-equal-to-k.rs
@@ -0,0 +1,21 @@
+// 2311. longest-binary-subsequence-less-than-or-equal-to-k
+// https://leetcode.com/problems/longest-binary-subsequence-less-than-or-equal-to-k
+
+impl Solution {
+    pub fn longest_subsequence(s: String, k: i32) -> i32 {
+        let mut sm = 0;
+        let mut cnt = 0;
+        let bits = (k as f64).log2() as usize + 1;
+        for (i, ch) in s.chars().rev().enumerate() {
+            if ch == '1' {
+                if i < bits && sm + (1 << i) <= k {
+                    sm += 1 << i;
+                    cnt += 1;
+                }
+            } else {
+                cnt += 1;
+            }
+        }
+        cnt
+    }
+}
