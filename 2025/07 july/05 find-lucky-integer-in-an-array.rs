fa54711d9c4cd73c9a20087e4b653af0bf53309f
Pritam Nikam <pritam.nikam@gmail.com>
2025-07-05 22:35:33 +0530

    Update 05 find-lucky-integer-in-an-array.rs

--- 2025/07 july/05 find-lucky-integer-in-an-array.rs
+++ 2025/07 july/05 find-lucky-integer-in-an-array.rs
@@ -1,16 +1,16 @@
 // 1394. find-lucky-integer-in-an-array
 // https://leetcode.com/problems/find-lucky-integer-in-an-array
 
+use itertools::Itertools;
+
 impl Solution {
     pub fn find_lucky(arr: Vec<i32>) -> i32 {
-        arr.into_iter()
-            .fold([0; 500], |mut f, num| {
-                f[num as usize - 1] += 1;
-                f
-            })
+        arr.iter()
+            .counts()
             .into_iter()
-            .enumerate()
-            .rfind(|(i, v)|  *v - *i as i32 == 1)
-            .map_or(-1, |(_, v)| v)
+            .filter(|(k, v)| **k == *v as i32)
+            .map(|(k, _)| *k)
+            .max()
+            .unwrap_or(-1)
     }
 }
