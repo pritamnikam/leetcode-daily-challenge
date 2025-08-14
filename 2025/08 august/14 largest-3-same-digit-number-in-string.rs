bccd8f5bcbdfbe9b77a133e40f52bd7ccdee263e
Pritam Nikam <pritam.nikam@gmail.com>
2025-08-14 08:45:12 +0530

    Create 14 largest-3-same-digit-number-in-string.rs

---
+++ 2025/08 august/14 largest-3-same-digit-number-in-string.rs
@@ -0,0 +1,19 @@
+// 2254. largest-3-same-digit-number-in-string
+// https://leetcode.com/problems/largest-3-same-digit-number-in-string
+
+impl Solution {
+    pub fn largest_good_integer(num: String) -> String {
+        let mut best = String::new();
+        let bytes = num.as_bytes();
+        for i in 0..bytes.len() - 2 {
+            if bytes[i] == bytes[i + 1] && bytes[i] == bytes[i + 2] {
+                let cur = &num[i..i + 3];
+                if cur > &best {
+                    best = cur.to_string();
+                }
+            }
+        }
+
+        best
+    }
+}
