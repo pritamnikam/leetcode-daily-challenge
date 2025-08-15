a1d0e7cdf348f28f6b4f3c1c881379527fc74413
Pritam Nikam <pritam.nikam@gmail.com>
2025-08-15 15:07:51 +0530

    Create 15 power-of-four.rs

---
+++ 2025/08 august/15 power-of-four.rs
@@ -0,0 +1,8 @@
+// 342. power-of-four
+// https://leetcode.com/problems/power-of-four
+
+impl Solution {
+    pub fn is_power_of_four(n: i32) -> bool {
+        n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0
+    }
+}
