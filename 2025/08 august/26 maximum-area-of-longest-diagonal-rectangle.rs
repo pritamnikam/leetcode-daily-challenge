cb3f55e40396798f79ea79f9a0ae5a0c1a8a3431
Pritam Nikam <pritam.nikam@gmail.com>
2025-08-26 06:57:17 +0530

    Update 26 maximum-area-of-longest-diagonal-rectangle.rs

--- 2025/08 august/26 maximum-area-of-longest-diagonal-rectangle.rs
+++ 2025/08 august/26 maximum-area-of-longest-diagonal-rectangle.rs
@@ -5,18 +5,16 @@ impl Solution {
     pub fn area_of_max_diagonal(dimensions: Vec<Vec<i32>>) -> i32 {
         let mut max_dia_sq = 0;
         let mut max_area = 0;
-
         for dim in dimensions {
             let l = dim[0];
             let w = dim[1];
-
             let dia_sq = l * l + w * w;
             let area = l * w;
             if dia_sq > max_dia_sq {
                 max_dia_sq = dia_sq;
                 max_area = area;
-            } else {
-                max_area = max_area.max(area);
+            } else if dia_sq == max_dia_sq {
+                max_area = std::cmp::max(max_area, area);
             }
         }
         max_area
