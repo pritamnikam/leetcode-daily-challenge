4a62bc2ea85bcc59e0e575b8de2bb1fff83aaf6c
Pritam Nikam <pritam.nikam@gmail.com>
2025-08-29 06:02:51 +0530

    Create 29 alice-and-bob-playing-flower-game.rs

---
+++ 2025/08 august/29 alice-and-bob-playing-flower-game.rs
@@ -0,0 +1,8 @@
+// alice-and-bob-playing-flower-game
+// https://leetcode.com/problems/alice-and-bob-playing-flower-game
+
+impl Solution {
+    pub fn flower_game(n: i32, m: i32) -> i64 {
+        (m as i64) * (n as i64) / 2
+    }
+}
