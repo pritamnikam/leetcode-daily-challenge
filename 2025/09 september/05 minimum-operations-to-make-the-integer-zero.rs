44b073444a66ac92a1f81b985f90b1aff75b7e62
Pritam Nikam <pritam.nikam@gmail.com>
2025-09-05 18:14:11 +0530

    Create 05 minimum-operations-to-make-the-integer-zero.rs

---
+++ 2025/09 september/05 minimum-operations-to-make-the-integer-zero.rs
@@ -0,0 +1,17 @@
+// 2749. minimum-operations-to-make-the-integer-zero
+// https://leetcode.com/problems/minimum-operations-to-make-the-integer-zero
+
+impl Solution {
+    pub fn make_the_integer_zero(num1: i32, num2: i32) -> i32 {
+        let mut k: i64 = 1;
+        loop {
+            let x: i64 = num1 as i64 - num2 as i64 * k;
+            if x < k {
+                return -1;
+            } else if k >= x.count_ones() as i64 {
+                return k as i32;
+            }
+            k += 1;
+        }
+    }
+}
