12ff6488fbdfd1d8eece9ff00d102425d0bb661d
Pritam Nikam <pritam.nikam@gmail.com>
2025-11-08 09:08:48 +0530

    Create 08 minimum-one-bit-operations-to-make-integers-zero.rs

---
+++ 2025/11 november/08 minimum-one-bit-operations-to-make-integers-zero.rs
@@ -0,0 +1,18 @@
+// minimum-one-bit-operations-to-make-integers-zero
+// https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero
+
+impl Solution {
+    pub fn minimum_one_bit_operations(n: i32) -> i32 {
+        let mut result = 0;
+        let mut i = 0;
+
+        while (1 << i) <= n {
+            if n & (1 << i) != 0 {
+                result = (1 << (i + 1)) - 1 - result;
+            }
+            i += 1;
+        }
+
+        result
+    }
+}
