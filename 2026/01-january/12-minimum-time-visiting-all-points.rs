1dc81076d7b888d124987936d111f19f0938a1b0
Pritam Nikam <pritam.nikam@gmail.com>
2026-01-12 05:57:11 +0530

    Create 12-minimum-time-visiting-all-points.rs

---
+++ 2026/01-january/12-minimum-time-visiting-all-points.rs
@@ -0,0 +1,16 @@
+// minimum-time-visiting-all-points
+// https://leetcode.com/problems/minimum-time-visiting-all-points
+
+impl Solution {
+    pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
+        let mut ans: i32 = 0;
+
+        for i in 1..points.len() {
+            let dx = (points[i][0] - points[i-1][0]).abs();
+            let dy = (points[i][1] - points[i-1][1]).abs();
+            ans += dx.max(dy);
+        }
+
+        ans
+    }
+}
